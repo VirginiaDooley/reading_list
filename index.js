8a505f99778b40a5449ea20e374402217f9cb98b
Virginia Dooley <vcdooley@gmail.com>
2019-10-25 17:35:38 +0100

    book search with options

--- index.js
+++ index.js
@@ -12,10 +12,32 @@ module.exports = server;
 
 prompt.start();
 
+var options = {
+    field: 'title',
+    offset: 0,
+    limit: 5,
+    type: 'books',
+    order: 'relevance',
+    lang: 'en'
+};
+
 prompt.get(['book_title'], function (err, result) {
     if (err) { return onErr(err); }
     console.log('Command-line input received:');
     console.log('  I want to find: ' + result.book_title);
+
+    books.search(result.book_title, options, function(error, results) {
+        if ( ! error ) {
+          results.map(book => {
+            console.log('Title: ' + book.title)
+            console.log('Author(s): ' + book.authors)
+            console.log('Publisher: ' + book.publisher)
+          })
+        } else {
+            console.log(error);
+        }
+    });
+
 });
 
 function onErr(err) {
