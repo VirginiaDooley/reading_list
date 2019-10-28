e2cf153c5e76dc1adbca25cbc46857e78cbb13cd
Virginia Dooley <vcdooley@gmail.com>
2019-10-28 13:46:26 +0000

    reformat and add exit condition

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ server.use(bodyParser.json());
 server.listen()
 module.exports = server;
 
+
 prompt.start(console.log('Enter a book name or subject'));
 
 prompt.get(['book_search'], async function (err, result) {
@@ -35,18 +36,21 @@ prompt.get(['book_search'], async function (err, result) {
           console.log('Title: ' + book.title)
           console.log('Author(s): ' + book.authors)
           console.log('Publisher: ' + book.publisher)
-          console.log('Choose a book number to add it to your reading list')
+          console.log('Choose a book number 1-5 to add it to your reading list')
         })
 
         const chooseBook = await prompt.get(['index'], async function (err, result) {
           if (err) { return onErr(err); }
-          console.log('  I want to add book #' + result.index + ' to my reading list');
           const index = result.index
-          if (index <= 5) {
+          if (index === 'exit') {
+            console.log('goodbye!');
+            // exit!
+          } else if (index <= 5) {
             addBooks(index)
+            // return to menu choices to add another book or search again
           } else {
             console.log('Please choose 1-5')
-            // return to addBooks
+            // return to index prompt
           }
         })
 
@@ -56,6 +60,7 @@ prompt.get(['book_search'], async function (err, result) {
           reading_list.push(chosen_book)
           let bookshelf = reading_list.map(book => book.title)
           console.log("Your reading list currently includes: " + bookshelf)
+          
           // add menu with prompts to search again and print reading list again
         }
 
@@ -81,9 +86,4 @@ const printList = () => {
     console.log("Your reading list is currently empty. Search and select books to add.")
     // function to start search
   }
-
 }
-
-// create a list of conditionals to guide the menu
-// would you like to search, press 1, etc...
-// or switch case https://stackoverflow.com/questions/24464404/how-to-readline-infinitely-in-node-js
