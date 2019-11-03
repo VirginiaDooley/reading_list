84cd2622d3f8be1bf7fcb8780b4f77d286455025
Virginia Dooley <vcdooley@gmail.com>
2019-11-03 11:32:34 +0000

    refactor search into a function and call function

--- index.js
+++ index.js
@@ -3,32 +3,40 @@ const server = express();
 const bodyParser = require('body-parser');
 const books = require('google-books-search');
 const prompt = require('prompt');
+const nodemon = require('nodemon')
 const port = process.env.PORT || 5000;
+
+const reading_list = []
 
 server.use(bodyParser.urlencoded({ extended: false }));
 server.use(bodyParser.json());
 server.listen()
 module.exports = server;
 
+prompt.start(
+  console.log("Welcome to Reading List. Enter 1 to search books; 2 to print your current list.")
+)
 
-prompt.start(console.log('Enter a book name or subject'));
+search()
 
-prompt.get(['book_search'], async function (err, result) {
+//start of search function
+function search(){
+
+  var options = {
+   offset: 0,
+   limit: 5,
+   type: 'books',
+   order: 'relevance',
+   lang: 'en'
+ }
+//ask for user input for book search
+  prompt.get(['book_search'], async function (err, result) {
     if (err) { return onErr(err); }
       console.log('  I want to search for: ' + result.book_search)
 
- try {
-   var options = {
-    offset: 0,
-    limit: 5,
-    type: 'books',
-    order: 'relevance',
-    lang: 'en'
-  };
+//search books
+  books.search(result.book_search, options, async function(error, results) {
 
-  const reading_list = []
-
-    books.search(result.book_search, options, async function(error, results) {
       if ( ! error ) {
         const books = results
         books.map((book, index) => {
@@ -41,6 +49,9 @@ prompt.get(['book_search'], async function (err, result) {
 
         const chooseBook = await prompt.get(['index'], async function (err, result) {
           if (err) { return onErr(err); }
+          // const book = books.find(book => book.id === parseInt(req.params.id))
+          // if (!book) res.status.(404).send('The book was not found') //404
+          // res.send(book)
           const index = result.index
           if (index === 'exit') {
             console.log('goodbye!');
@@ -50,7 +61,7 @@ prompt.get(['book_search'], async function (err, result) {
             // return to menu choices to add another book or search again
           } else {
             console.log('Please choose 1-5')
-            // return to index prompt
+          addBooks()
           }
         })
 
@@ -60,7 +71,8 @@ prompt.get(['book_search'], async function (err, result) {
           reading_list.push(chosen_book)
           let bookshelf = reading_list.map(book => book.title)
           console.log("Your reading list currently includes: " + bookshelf)
-          
+          // reading_list.push(bookshelf)
+
           // add menu with prompts to search again and print reading list again
         }
 
@@ -72,12 +84,12 @@ prompt.get(['book_search'], async function (err, result) {
       console.log(err);
       return 1;
     }
-  }
-  catch (err) {
-    console.log(err);
-    res.status(500).send("something went bad");
-  }
-})
+  })
+  // catch (err) {
+  //   console.log(err);
+  //   res.status(500).send("something went bad");
+  // }
+}
 
 const printList = () => {
   if (reading_list.length > 0) {
