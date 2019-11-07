a2fc9ed380e96f102c812fec5d3b6ab97b3210d3
Virginia Dooley <vcdooley@gmail.com>
2019-11-07 18:59:49 +0000

    test console.log

--- index.test.js
+++ index.test.js
@@ -1,10 +1,16 @@
-import each from 'jest-each'
 
- describe('search', () = {
-   each`
-     input     | expectedResult
-     ${search}   | ${'search'}
-   `.test('converts $input to $expectedResult', ({ input, expectedResult }) => {
-    expect(search(input)).toBe(expectedResult)
+global.console = {
+    log: jest.fn(),
+    info: jest.fn(),
+    error: jest.fn()
+  }
+
+describe('Tests my console.log', () => {
+    it('should console a message', () => {
+      console.log('My test is working with console.log')
+  
+      expect(global.console.log).toHaveBeenCalledWith(
+        'My test is working with console.log'
+      )
+    })
   })
-})
