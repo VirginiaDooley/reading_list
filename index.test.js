fb0a798ba7cdb4e8a5224e6e69dc1999d9d1bacc
Virginia Dooley <vcdooley@gmail.com>
2019-11-18 11:34:54 +0000

    add test for results

--- index.test.js
+++ index.test.js
@@ -3,14 +3,19 @@ global.console = {
     log: jest.fn(),
     info: jest.fn(),
     error: jest.fn()
-  }
+  };
 
 describe('Tests my console.log', () => {
     it('should console a message', () => {
-      console.log('My test is working with console.log')
+      console.log('My test is working with console.log');
   
       expect(global.console.log).toHaveBeenCalledWith(
         'My test is working with console.log'
-      )
-    })
-  })
+      );
+    });
+  });
+
+  const results = require('./index.js');
+  test('book search yields at least 3 results', () => {
+    expect(results.length).toBe(3);
+  });
